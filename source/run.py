d3efc81f77e796d09c677c54902d32580dd562af
jose_leiva <jseleivaf686@gmail.com>
2024-10-05 01:04:00 +0200

    add transformed data

--- source/run.py
+++ source/run.py
@@ -14,3 +14,45 @@ from data_processing import *
 from utils import *
 from models import *
  
+
+path_transformed_df = '/media/jose/A63C16883C1654211/Proyectos/Sentiment-Wellness-Tool/data/kmeans_data/data.csv' 
+# print("j")
+# raw_data = pd.read_csv('/media/jose/A63C16883C1654211/Proyectos/Sentiment-Wellness-Tool/data/raw_data/data.csv')
+# print("pp")
+# features, labels = get_features(raw_data)
+
+
+# dataframe = pd.DataFrame(features)
+
+# dataframe.to_csv(path_transformed_df + '/data.csv')
+
+# #dataframe = pd.read_csv(path_transformed_df)
+
+dataframe = pd.read_csv(path_transformed_df)
+correlation = dataframe.corr()
+
+threshold = 0.8
+filtered_correlation = correlation[(correlation >= threshold) | (correlation <= -threshold)]
+plt.figure(figsize=(12, 10))  # Set the figure size to accommodate many features
+sns.heatmap(filtered_correlation, annot=False, cmap='coolwarm', fmt=".2f", square=True)
+plt.title('Correlation Matrix Heatmap')
+plt.xlabel('Features')
+plt.ylabel('Features')
+plt.show()
+
+print('visualize')
+
+path_reduced_df = '/media/jose/A63C16883C1654211/Proyectos/Sentiment-Wellness-Tool/data/kmeans_data/reduced_data.csv'
+to_drop = set()  # Set to hold features to drop
+
+for i in range(len(correlation.columns)):
+    for j in range(i):
+        if correlation.iloc[i, j] > threshold:
+            colname = correlation.columns[i]  # Get column name
+            to_drop.add(colname)  # Add to drop set
+print("aaaaaaaaa")
+dataframe_reduced = dataframe.drop(columns=to_drop)
+
+dataframe_reduced.to_csv(path_reduced_df)
+print("skdnskd")
+#dataframe.to_csv(path_transformed_df + '\data.csv')
